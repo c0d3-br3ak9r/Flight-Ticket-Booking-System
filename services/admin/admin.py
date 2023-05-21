890473aa338e38e1ea358964151b127b49fbed94
c0d3-br3ak9r <103585603+c0d3-br3ak9r@users.noreply.github.com>
2023-05-21 15:31:25 +0530

    Refactored admin

--- services/admin/admin.py
+++ services/admin/admin.py
@@ -1,22 +1,24 @@
 import datetime
-from services import validation
+from services import session
 import bcrypt
 from services.admin.admin_db import AdminDB
 from services.booking.booking_db import BookingDB
 
+
 ''' Returns true if the admin credentials are valid '''
-def is_valid_user(username, password):
-    if ( validation.is_valid_username(username) and 
-        validation.is_valid_password(password) ):
-        db = AdminDB()
-        lst = db.get_admin_password(username)
-        if ( not lst ):
-            return 0
-        user_id = lst[0]
-        hashed_pw = lst[1]
-        if ( hashed_pw and bcrypt.checkpw(bytes(password, 'utf-8'), bytes(hashed_pw, 'utf-8')) ):
-            return user_id
-    return 0
+def login_admin(username, password):
+    db = AdminDB()
+    lst = db.get_admin_password(username)
+    if ( not lst ):
+        return 2
+    user_id = lst[0]
+    hashed_pw = lst[1]
+    if ( hashed_pw and bcrypt.checkpw(bytes(password, 'utf-8'), bytes(hashed_pw, 'utf-8')) ):
+        if ( session.create_user_session(user_id) ):
+            return 1
+        return 3
+    return 2
+
 
 ''' Validates the admin session '''
 def validate_session(session_id):
--- services/service.py
+++ services/service.py
@@ -7,8 +7,10 @@ from services.booking import booking
 
 ''' To create a new user '''
 def create_user(payload):
+
     username = payload.get("username")
     password = payload.get("password")
+
     if ( validation.is_valid_username(username) and validation.is_valid_password(password) ):
         return user.create_user(username, password)
     return 2
@@ -17,12 +19,15 @@ def create_user(payload):
 ''' To get flight details '''
 def get_flights(session_id, payload):
     if ( session_id and user.validate_session(session_id) ):
+
         date = payload.get('date')
         time = payload.get('time')
         flight = payload.get('flight')
+
         flight = flight if flight and str.isalnum(flight) else None
         date = date if validation.is_valid_date(date) else None
         time = time if validation.is_valid_time(time) else None
+
         resp = user.get_flights_data(flight, date, time)
         return {
             "count" : len(resp),
@@ -34,14 +39,17 @@ def get_flights(session_id, payload):
 ''' To book flight ticket '''
 def book_flight(session_id, payload):
     if ( session_id and user.validate_session(session_id) ):
+
         flight = payload.get("flight_no")
         date = payload.get("date")
         time = payload.get("time")
         seat_no = payload.get("seat_no")
+
         if ( str.isalnum(flight) and validation.is_valid_date(date)
             and validation.is_valid_time(time) 
             and ( not seat_no or ( 1 <= seat_no <= 60 ) ) ):
-            return booking.book_flight(session_id, flight, date, time, seat_no)
+                
+                return booking.book_flight(session_id, flight, date, time, seat_no)
         return 2
     return 4
 
@@ -49,7 +57,9 @@ def book_flight(session_id, payload):
 ''' To get all bookings made by the user '''
 def get_bookings(session_id):
     if ( session_id and user.validate_session(session_id) ):
+
         resp = booking.get_bookings(session_id)
+
         return {
             "count" : len(resp),
             "flights" : resp
@@ -59,8 +69,10 @@ def get_bookings(session_id):
 
 ''' To authenticate user '''
 def authenticate_user(payload):
+
     username = payload.get("username")
     password = payload.get("password")    
+
     if ( validation.is_valid_username(username) and validation.is_valid_password(password) ):
         return user.login_user(username, password)
     return 2
@@ -73,13 +85,25 @@ def logout_user(session_id):
     return 4
 
 
+''' To authenticate admin '''
+def authenticate_admin(payload):
+
+    username = payload.get("username")
+    password = payload.get("password")   
+
+    if ( validation.is_valid_username(username) and validation.is_valid_password(password) ):
+        return admin.login_admin(username, password)
+    return 2
+
+
 ''' To create a flight '''
 def create_flight(session_id, flight_no, airline, source, destination):
     if ( session_id and admin.validate_session(session_id) ):
         if ( str.isalnum(flight_no) and validation.strings_and_spaces(airline) and
             validation.strings_and_spaces(source) and validation.strings_and_spaces(destination) ):
-            return flight.create_flight(flight_no, airline, source, destination)
-    return "Forbidden", 403
+                
+                return flight.create_flight(flight_no, airline, source, destination)
+    return 4
 
 
 ''' To create a flight timing '''
@@ -87,7 +111,8 @@ def create_flight_timing(session_id, flight_no, date, time):
     if ( session_id and admin.validate_session(session_id) ):
         if ( str.isalnum(flight_no) and validation.is_valid_date(date) and
             validation.is_valid_time(time) ):
-            return flight.create_flight_timing(flight_no, date, time)
+
+                return flight.create_flight_timing(flight_no, date, time)
         return 2
     return 4
 
@@ -114,12 +139,15 @@ def remove_flight_timing(session_id, flight_no, date, time):
 ''' To get all bookings '''
 def get_all_bookings(session_id, payload):
     if ( session_id and admin.validate_session(session_id) ):
+
         flight = payload.get("flight_no")
         date = payload.get("date")
         time = payload.get("time")
+
         flight = flight if str.isalnum(flight) else None
         date = date if validation.is_valid_date(date) else None
         time = time if validation.is_valid_time(time) else None
+        
         resp = admin.get_bookings(flight, date, time)
         return {
             "count" : len(resp),
